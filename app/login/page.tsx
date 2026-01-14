84e7fe6f90a7ff821c065b0207e396460534ff3b
Dave Ubuntu <dboland77@gmail.com>
2026-01-14 10:54:51 +0000

    Added google one tap signin page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,18 @@
 import { login, signup } from './actions';
+import OneTapComponent from './GoogleOneTap';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+      <OneTapComponent />
+    </>
   );
 }
