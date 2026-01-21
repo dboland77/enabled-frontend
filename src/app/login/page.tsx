14d27a13e5a913125914e636237019a4f33945fc
Dave Ubuntu <dboland77@gmail.com>
2026-01-21 10:21:53 +0000

    Made the login form a client component and added react form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,47 @@
+'use client';
 import { login, signup } from './actions';
 import OneTapComponent from './GoogleOneTap';
 import * as Yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-
-const LoginSchema = Yup.object().shape({
-  email: Yup.string().required('Email is required').email('Email must be a valid email address'),
-  password: Yup.string().required('Password is required'),
-});
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 export default function LoginPage() {
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+    password: Yup.string().required('Password is required'),
+  });
+  const defaultValues = {
+    email: 'employee@company.com',
+    password: 'demo',
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const renderHead = (
+    <Stack spacing={2} sx={{ mb: 5 }}>
+      <Typography variant="h4">Sign in to enableD</Typography>
+
+      <Stack direction="row" spacing={0.5}>
+        <Typography variant="body2">New user?</Typography>
+
+        <button formAction={login}>Create an account</button>
+      </Stack>
+    </Stack>
+  );
+
   return (
     <>
       <form>
