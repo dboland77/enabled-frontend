2a7495a62cb8c7e06d11cde766c0b9df320f67a4
Dave Ubuntu <dboland77@gmail.com>
2026-02-02 14:16:25 +0000

    Adding other referenced components

---
+++ src/components/editor/editor.tsx
@@ -0,0 +1,56 @@
+import '../../utils/highlight';
+
+import ReactQuill from 'react-quill';
+
+import { alpha } from '@mui/material/styles';
+
+import { EditorProps } from './types';
+import { StyledEditor } from './styles';
+import Toolbar, { formats } from './toolbar';
+
+export default function Editor({
+  id = 'enabled-quill',
+  error,
+  simple = false,
+  helperText,
+  placeholder,
+  sx,
+  ...other
+}: EditorProps) {
+  const modules = {
+    toolbar: {
+      container: `#${id}`,
+    },
+    history: {
+      delay: 500,
+      maxStack: 100,
+      userOnly: true,
+    },
+    syntax: true,
+    clipboard: {
+      matchVisual: false,
+    },
+  };
+
+  return (
+    <>
+      <StyledEditor
+        sx={{
+          ...(error && {
+            border: (theme) => `solid 1px ${theme.palette.error.main}`,
+            '& .ql-editor': {
+              bgcolor: (theme) => alpha(theme.palette.error.main, 0.08),
+            },
+          }),
+          ...sx,
+        }}
+      >
+        <Toolbar id={id} simple={simple} />
+
+        <ReactQuill modules={modules} formats={formats} placeholder={placeholder} {...other} />
+      </StyledEditor>
+
+      {helperText && helperText}
+    </>
+  );
+}
