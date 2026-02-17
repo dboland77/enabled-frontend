ff2140ac9e62ea660d8c4e3341488853837f2892
Dave Ubuntu <dboland77@gmail.com>
2026-02-17 14:26:26 +0000

    Auth Modern Layout now being applied

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,10 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import React from 'react';
+import AuthModernLayout from '@/app/auth/layouts/AuthModernLayout';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
-export const metadata: Metadata = {
-  title: 'enableD: Reasonable Adjustments',
-  description: 'enableD - disability adjustments system',
-  keywords: 'react,material,kit,application,dashboard,admin,template',
+type Props = {
+  children: React.ReactNode;
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-    </html>
-  );
+export default function AuthLayout({ children }: Props) {
+  return <AuthModernLayout>{children}</AuthModernLayout>;
 }
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -32,22 +32,26 @@ const Logo = forwardRef<HTMLDivElement, LogoProps>(
         }}
         {...other}
       >
-        <defs>
-          <linearGradient id="BG1" x1="100%" x2="50%" y1="9.946%" y2="50%">
-            <stop offset="0%" stopColor={PRIMARY_DARK} />
-            <stop offset="100%" stopColor={PRIMARY_MAIN} />
-          </linearGradient>
+        {/* All gradients must be inside <svg> */}
+        <svg width="100" height="50">
+          <defs>
+            <linearGradient id="BG1" x1="100%" x2="50%" y1="9.946%" y2="50%">
+              <stop offset="0%" stopColor={PRIMARY_DARK} />
+              <stop offset="100%" stopColor={PRIMARY_MAIN} />
+            </linearGradient>
 
-          <linearGradient id="BG2" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor={PRIMARY_LIGHT} />
-            <stop offset="100%" stopColor={PRIMARY_MAIN} />
-          </linearGradient>
+            <linearGradient id="BG2" x1="50%" x2="50%" y1="0%" y2="100%">
+              <stop offset="0%" stopColor={PRIMARY_LIGHT} />
+              <stop offset="100%" stopColor={PRIMARY_MAIN} />
+            </linearGradient>
 
-          <linearGradient id="BG3" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor={PRIMARY_LIGHT} />
-            <stop offset="100%" stopColor={PRIMARY_MAIN} />
-          </linearGradient>
-        </defs>
+            <linearGradient id="BG3" x1="50%" x2="50%" y1="0%" y2="100%">
+              <stop offset="0%" stopColor={PRIMARY_LIGHT} />
+              <stop offset="100%" stopColor={PRIMARY_MAIN} />
+            </linearGradient>
+          </defs>
+          {/* All gradients must be inside <svg> */}
+        </svg>
         <svg height="50" width="100">
           <text x="0" y="20" fill={NICE_BLUE} letterSpacing="2" fontSize="22">
             enableD
